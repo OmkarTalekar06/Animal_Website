a215b4fe168a7c938f8bbde491bb18998c9a888c
OmkarTalekar06 <sanskrutitalekar1982@gmail.com>
2025-06-14 14:02:10 +0530

    added blog

--- quiz.js
+++ quiz.js
@@ -21,7 +21,7 @@
     } else {
         document.getElementById("park").style.backgroundColor = "#e97474"
     }
-    if (document.getElementById("AP").checked) {
+    if (document.getElementById("tree").checked) {
         marks += 5;
         document.getElementById("tree").style.backgroundColor = "lightgreen";
     } else {
